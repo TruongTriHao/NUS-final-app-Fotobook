cdc37461f4655c36b0ac6fc80027f2a39af871b0
haott <hao.truongmywtag@hcmut.edu.vn>
2026-07-03 08:18:41 +0700

    fix: use cn in TextInput to merge

--- frontend/src/components/ui/TextInput.tsx
+++ frontend/src/components/ui/TextInput.tsx
@@ -1,8 +1,13 @@
+import { cn } from "../../utils/cn";
+
 export function TextInput(props: React.InputHTMLAttributes<HTMLInputElement>) {
   return (
     <input
       {...props}
-      className="border-2 border-neutral-200 p-1.5 md:p-3 mx-2 md:mx-4 rounded-sm md:min-w-54.5 text-xs md:text-base"
+      className={cn(
+        "border-2 border-neutral-200 p-1.5 md:p-3 mx-2 md:mx-4 rounded-sm md:min-w-54.5 text-xs md:text-base",
+        props.className,
+      )}
     />
   );
 }
