ba0eb63b31a1fdb77583a840951c51ef379ed055
haott <hao.truongmywtag@hcmut.edu.vn>
2026-07-06 16:03:26 +0700

    add more className to InputField

--- frontend/src/components/ui/InputField.tsx
+++ frontend/src/components/ui/InputField.tsx
@@ -4,18 +4,23 @@ export function InputField({
   label,
   htmlFor,
   children,
-  className,
+  outerClassName,
+  labelClassName,
 }: {
   label: string;
   htmlFor: string;
   children?: React.ReactNode;
-  className?: string;
+  outerClassName?: string;
+  labelClassName?: string;
 }) {
   return (
-    <div className="flex flex-col my-1">
+    <div className={cn("flex flex-col my-1", outerClassName)}>
       <label
         htmlFor={htmlFor}
-        className={cn("font-bold text-xs md:text-base mx-2 md:mx-4", className)}
+        className={cn(
+          "font-bold text-xs md:text-base mx-2 md:mx-4",
+          labelClassName,
+        )}
       >
         {label}
       </label>
