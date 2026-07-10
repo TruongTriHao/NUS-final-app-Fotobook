a1c49eba93d7135aee0fac6b666d32847a010dcb
haott <hao.truongmywtag@hcmut.edu.vn>
2026-07-10 13:02:23 +0700

    add Loading and Alert to LoginPage, move Title and SocialLogin inside LoginForm

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,20 +1,5 @@
-import { Link } from "react-router-dom";
-import { AuthTitle } from "../components/auth/AuthTitle";
 import { LoginForm } from "../components/auth/LoginForm";
-import { SocialLogin } from "../components/auth/SocialLogin";
 
 export function LoginPage() {
-  return (
-    <>
-      <AuthTitle>Fotobook Login</AuthTitle>
-      <SocialLogin />
-      <LoginForm />
-      <Link
-        className="text-slate-500 my-2.5 md:my-5 text-xs md:text-base hover:opacity-70 active:text-slate-700"
-        to="/signup"
-      >
-        Create an account
-      </Link>
-    </>
-  );
+  return <LoginForm />;
 }
