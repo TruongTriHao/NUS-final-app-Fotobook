b51688473080f8e05679125e2157ec4a743c071a
haott <hao.truongmywtag@hcmut.edu.vn>
2026-07-13 02:46:45 +0700

    add onClick to Photo and Album for popup modal

--- frontend/src/components/ui/Album.tsx
+++ frontend/src/components/ui/Album.tsx
@@ -6,23 +6,28 @@ export function Album({
   className,
   imageClassName,
   alt,
+  onClick,
 }: {
   src: [string, string | null, string | null];
   className?: string;
   imageClassName?: string;
   alt: string;
+  onClick?: () => void;
 }) {
   return (
-    <div className={cn("relative shrink-0 m-1", className)}>
+    <div className={cn("relative shrink-0 m-1", className)} onClick={onClick}>
       <Photo
         src={src[0]}
-        className={cn("absolute inset-0 w-full -rotate-6", imageClassName)}
+        className={cn("absolute inset-0 w-full z-20", imageClassName)}
         alt={alt}
       />
       {src[1] && (
         <Photo
           src={src[1]}
-          className={cn("absolute inset-0 w-full rotate-3", imageClassName)}
+          className={cn(
+            "absolute inset-0 w-full rotate-3 z-10",
+            imageClassName,
+          )}
           alt={alt}
         />
       )}
@@ -30,7 +35,7 @@ export function Album({
       {src[2] && (
         <Photo
           src={src[2]}
-          className={cn("absolute inset-0 w-full", imageClassName)}
+          className={cn("absolute inset-0 w-full rotate-6", imageClassName)}
           alt={alt}
         />
       )}
--- frontend/src/components/ui/Photo.tsx
+++ frontend/src/components/ui/Photo.tsx
@@ -4,19 +4,23 @@ export function Photo({
   src,
   className,
   alt,
+  onClick,
 }: {
   src: string;
   className?: string;
   alt: string;
+  onClick?: () => void;
 }) {
   return (
     <img
       className={cn(
-        "aspect-square object-cover rounded-sm shrink-0",
+        "aspect-square object-cover rounded-sm shrink-0 select-none",
         className,
       )}
       src={src}
       alt={alt}
+      onClick={onClick}
+      draggable={false}
     />
   );
 }
