06889cc62fa0783778d8bf072ee68d77e167d88c
haott <hao.truongmywtag@hcmut.edu.vn>
2026-07-13 03:18:12 +0700

    add cursor-pointer to TabButton

--- frontend/src/components/main/TabButton.tsx
+++ frontend/src/components/main/TabButton.tsx
@@ -10,7 +10,7 @@ export function TabButton({
   const activeStyle =
     "text-white bg-indigo-800 font-bold border-2 border-indigo-800 rounded-xs p-2 text-xs md:text-base";
   const inactiveStyle =
-    "text-indigo-800 bg-white font-bold border-2 border-indigo-800 rounded-xs p-2 text-xs md:text-base hover:opacity-70";
+    "text-indigo-800 bg-white font-bold border-2 border-indigo-800 rounded-xs p-2 text-xs md:text-base hover:opacity-70 cursor-pointer";
 
   return (
     <button className={active ? activeStyle : inactiveStyle} onClick={onClick}>
