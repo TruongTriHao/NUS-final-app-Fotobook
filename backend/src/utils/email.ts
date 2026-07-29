acc3cd6158f902d8f062df6d5ff4ff37701c4db2
haott <hao.truongmywtag@hcmut.edu.vn>
2026-07-29 14:39:00 +0700

    change from using nodemailer to resend

--- backend/src/utils/email.ts
+++ backend/src/utils/email.ts
@@ -1,25 +1,23 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 
-const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:3000";
+const {
+  RESEND_API_KEY,
+  RESEND_FROM = "onboarding@resend.dev",
+  RESEND_TO,
+  CORS_ORIGIN = "http://localhost:3000",
+} = process.env;
 
-const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
-  secure: process.env.SMTP_SECURE === "true",
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
-});
+const resend = new Resend(RESEND_API_KEY);
 
 export async function sendVerificationEmail(
   email: string,
   token: string,
 ): Promise<void> {
   const url = `${CORS_ORIGIN}/verify-email/${token}`;
-  await transporter.sendMail({
-    from: process.env.RESEND_FROM || "onboarding@resend.dev",
-    to: process.env.RESEND_TO || email,
+
+  await resend.emails.send({
+    from: RESEND_FROM,
+    to: RESEND_TO || email,
     subject: "Verify your Fotobook Account",
     html: `Please click <a href="${url}">here</a> to verify your email.`,
   });
@@ -30,9 +28,10 @@ export async function sendPasswordResetEmail(
   token: string,
 ): Promise<void> {
   const url = `${CORS_ORIGIN}/reset-password/${token}`;
-  await transporter.sendMail({
-    from: process.env.RESEND_FROM || "onboarding@resend.dev",
-    to: process.env.RESEND_TO || email,
+
+  await resend.emails.send({
+    from: RESEND_FROM,
+    to: RESEND_TO || email,
     subject: "Reset your Fotobook Password",
     html: `Please click <a href="${url}">here</a> to reset your password.`,
   });
