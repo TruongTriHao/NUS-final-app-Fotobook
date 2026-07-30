2f09225f7b47374061e12d5354345ee3e60da7e4
haott <hao.truongmywtag@hcmut.edu.vn>
2026-07-30 10:38:37 +0700

    fix be able to use seed data image url

--- frontend/src/utils/url.ts
+++ frontend/src/utils/url.ts
@@ -2,10 +2,11 @@ export function getPublicIdFromCloudinarySignedUrl(url: string): string {
   const pathname = new URL(url).pathname;
   const marker = "/v1/";
   const index = pathname.indexOf(marker);
+  // For demonstration purposes, uncomment to throw error in production
   if (index === -1) {
-    if (import.meta.env.PROD) {
-      throw new Error("Invalid Cloudinary signed URL.");
-    }
+    // if (import.meta.env.PROD) {
+    //   throw new Error("Invalid Cloudinary signed URL.");
+    // }
     return url;
   }
   return decodeURIComponent(pathname.slice(index + marker.length));
